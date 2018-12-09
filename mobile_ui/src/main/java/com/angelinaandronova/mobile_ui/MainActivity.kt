4ba9a83d4f9e3d67715ff277f6ec7dfab3522b8c
angelina <a.andronova@gmail.com>
2018-12-09 14:22:49 +0100

    added mobile ui module and dependencies

---
+++ mobile_ui/src/main/java/com/angelinaandronova/mobile_ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.angelinaandronova.mobile_ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
